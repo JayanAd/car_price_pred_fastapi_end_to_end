4be4df8786f7a8a497c61d050642b60764608161
Jayan Adhikari <adhikarijayan1@gmail.com>
2025-09-21 11:57:39 +0545

    minor issue solved

--- app/core/exceptions.py
+++ app/core/exceptions.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 
 def register_exception_handler(app: FastAPI):
-    @app.add_exception_handler(Exception)
+    @app.exception_handler(Exception)
     async def unhandled_exception_handler(request: Request, exc: Exception):
         return JSONResponse(
             status_code = 500,
